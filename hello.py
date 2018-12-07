27fbf419dc15b3d80d84a5d7878a3551782e4315
Laurent Mallet <laurent.mallet@gmail.com>
2018-12-07 23:05:30 +0100

    wip

--- hello.py
+++ hello.py
@@ -1,6 +1,19 @@
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
+
+Entries = []
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "API de tonio (qui n'a pas honte)"
+
+@app.route("/entry/create", methods=["POST"])
+def createEntry():
+    """Create Entry"""
+    request_json = request.get_json()
+    return 'bite'
+
+@app.route("/<int:entry_id>", methods=["GET"])
+def entry(entry_id):
+    """test"""
+    return "total %d %s" % (entry_id)
